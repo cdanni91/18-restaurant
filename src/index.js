a92f6cf9deddacfa32009d2973ffc4ace2403735
cdanni91 <cdanni91@gmail.com>
2025-06-27 17:36:22 -0300

    Crate append functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+//import { create } from "domain"; // it breaks the serve no se pq aparecio
 import "./styles.css";
 
-console.log(greeting);
+function CreateHomeModule() {
+
+    const contentElement = document.querySelector(".content");
+
+
+    function logContent (content) {
+
+        console.log(content);
+    }
+
+    function createMainElement() {
+
+        const mainElement = document.createElement("div");
+        mainElement.classList.add("home");
+
+        return mainElement
+    }
+
+    function appendElementToContent(child) {
+
+        contentElement.appendChild(child);
+    }
+
+
+
+    return {logContent,
+            createMainElement,
+            appendElementToContent}
+}
+
+
+const home = CreateHomeModule();
+const mainElement = home.createMainElement();
+home.logContent(mainElement);
+home.appendElementToContent(mainElement);
+
