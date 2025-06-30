1dcf0a978ea4d9f6be482b35ab44f4f67ca745b1
cdanni91 <cdanni91@gmail.com>
2025-06-30 19:58:49 -0300

    Finish general style

--- src/contact.js
+++ src/contact.js
@@ -8,11 +8,10 @@ function CreateContactModule() {
     
 
     const contactElements = [   ["div","title","titleElement","Ponete en contacto con Emanuelito's"], 
-                                ["div","description", "descriptionElement","Whatsapp: 034423434"], 
+                                ["div","description", "descriptionElement",'Información de Contacto:\nDirección: Calle de los Sabores 1234, Barrio La Masa, Rosario, Santa Fe.\nTeléfono: (341) 555-PIZZA (555-74992)\nEmail: info@emanuelitos.com\nHorarios: Martes a Domingo, de 19:00 a 23:00 hs. (Lunes cerrado por amasado intensivo).'],
                                 ["div","hours","hoursElement","Lunes a Viernes: 09:00 a 18:00"]
                         ]
-
-        
+                        
 
     function executeContact() {
         // create the home element and append the home element
@@ -28,3 +27,5 @@ function CreateContactModule() {
 }
 
 export {CreateContactModule}
+
+
